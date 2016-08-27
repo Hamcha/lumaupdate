046f7a04da8d515e8dc23d8686ba30e81d51c368
Rhys Cox <rhys.cox@matchboxmobile.com>
2016-08-27 14:20:10 +0100

    Fixed version string check for arm9loaderhax.bin

--- source/version.cpp
+++ source/version.cpp
@@ -61,68 +61,31 @@ LumaVersion versionSvc() {
 }
 
 LumaVersion versionMemsearch(const std::string& path) {
-	const static char searchString[] = "Luma3DS v";
-	const static size_t searchStringLen = sizeof(searchString)/sizeof(char) - 1;
-
-	std::ifstream payloadFile(path, std::ios::binary | std::ios::ate);
+	std::ifstream payloadFile(path, std::ios::binary);
 	if (!payloadFile) {
 		logPrintf("Could not open existing payload, does it exists?\n");
-		return LumaVersion{};
+		return LumaVersion{ "", "", false };
 	}
-
-	/* Load entire file into local buffer */
-	size_t payloadSize = payloadFile.tellg();
-	payloadFile.seekg(0, std::ios::beg);
-	char* payloadData = (char*)std::malloc(payloadSize);
-	payloadFile.read(payloadData, payloadSize);
-	payloadFile.close();
 
 	logPrintf("Loaded existing payload in memory, searching for version number...\n");
-
-	size_t curProposedOffset = 0;
-	unsigned short curStringIndex = 0;
-	bool found = false;
-	std::string versionString = "";
-
-	// Byte-by-byte search. (memcmp might be faster?)
-	// Since " " (1st char) is only used once in the whole string we can search in O(n)
-	for (size_t offset = 0; offset < payloadSize - searchStringLen; ++offset) {
-		if (payloadData[offset] == searchString[curStringIndex]) {
-			if (curStringIndex == searchStringLen - 1) {
-				found = true;
-				break;
-			}
-			if (curStringIndex == 0) {
-				curProposedOffset = offset;
-			}
-			curStringIndex++;
-		}
-		else {
-			if (curStringIndex > 0) {
-				curStringIndex = 0;
-			}
-		}
+	std::string str((std::istreambuf_iterator<char>(payloadFile)), std::istreambuf_iterator<char>()); 
+	size_t pos = str.find("Luma3DS v"); 
+	if (pos == std::string::npos) { 
+		logPrintf("Could not find version number in payload.\n");
+		return LumaVersion{ "", "", false };
 	}
-
-	if (found) {
-		// Version is what comes after " v" and before " configuration"
-		curProposedOffset += searchStringLen;
-		size_t verOffset = curProposedOffset;
-		for (; verOffset < payloadSize; ++verOffset) {
-			if (payloadData[verOffset] == 'c' && payloadData[verOffset-1] == ' ') {
-				break;
-			}
-		}
-		// Get full version string
-		versionString = std::string(payloadData + curProposedOffset, verOffset - curProposedOffset - 1);
+	
+	size_t confPos = str.find("configuration", pos);
+	if (confPos == std::string::npos) {
+		logPrintf("Could not find version number in payload.\n");
+		return LumaVersion{ "", "", false };
 	}
+	
+	// Increment position to get past the 'Luma3DS v' string
+	pos += 9;
 
-	std::free(payloadData);
-
-	const size_t separator = versionString.find("-");
-	if (separator == std::string::npos) {
-		return LumaVersion{ versionString, "", false };
-	}
+	const std::string versionString = str.substr(pos, confPos - pos);
+	logPrintf("Found version string: %s\n", versionString);
 
 	LumaVersion version;
 	version.release = versionString.substr(0, separator);
