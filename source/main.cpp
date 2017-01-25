8492f4cbffa6f1511665410b7b49e1ed7c2047e9
Hamcha <zikyky@gmail.com>
2017-01-25 23:48:09 +0100

    Add support for embedding fonts to Cmakefile

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,20 @@
 #include <sf2d.h>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+	std::srand(std::time(NULL));
+
 	sf2d_init();
 	sf2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
 	sf2d_set_3D(1);
 	while (aptMainLoop()) {
+		hidScanInput();
+
+		if (hidKeysHeld() & KEY_START) {
+			break;
+		}
+
 		sf2d_swapbuffers();
 	}
 
